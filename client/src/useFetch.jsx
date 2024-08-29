603f81794ddd673145f76db45478cb71c8d3aba7
sinskiy <sinskiy@proton.me>
2024-08-29 18:26:58 +0300

    feat(client): add useFetch()

---
+++ client/src/useFetch.jsx
@@ -0,0 +1,39 @@
+import { useRef, useState } from "react";
+
+export default function useFetch() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const abortControllerRef = useRef(null);
+  async function fetchData(url, options) {
+    abortControllerRef.current?.abort();
+    abortControllerRef.current = new AbortController();
+
+    setIsLoading(true);
+
+    try {
+      const fetchArgs = [
+        import.meta.env.VITE_API_URL + url,
+        {
+          signal: abortControllerRef.current?.signal,
+          ...options,
+        },
+      ];
+      const response = await fetch(...fetchArgs);
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error);
+      }
+      setData(data);
+      setError(null);
+    } catch (err) {
+      setData(null);
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return { data, error, isLoading, fetchData };
+}
