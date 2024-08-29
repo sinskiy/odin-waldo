a155aaaee03e0c87b8c5f23cd3c3c2d29e08a641
sinskiy <sinskiy@proton.me>
2024-08-29 12:31:18 +0300

    feat(server): add targetGet

--- server/src/controllers/targetController.js
+++ server/src/controllers/targetController.js
@@ -1,1 +1,16 @@
-export async function targetGet(req, res) {}
+import { getMatchingTarget } from "../services/targetService.js";
+
+export async function targetGet(req, res, next) {
+  const { x, y } = req.query;
+  try {
+    const targetName = await getMatchingTarget(Number(x), Number(y));
+    if (!targetName) {
+      const error = new Error("Coords are incorrect.");
+      error.statusCode = 400;
+      throw error;
+    }
+    res.json(targetName);
+  } catch (err) {
+    next(err);
+  }
+}
