9492147e649b59f3c212e67b72788ed60d060a68
sinskiy <sinskiy@proton.me>
2024-08-29 17:45:16 +0300

    feat(client): add Target with actual data

--- client/src/TargetsForm.jsx
+++ client/src/TargetsForm.jsx
@@ -8,16 +8,8 @@ const TargetsForm = () => {
         src="leinster-coat-of-arms.svg"
         text="coat of arms of leinster"
       />
-      <Target
-        label="leinster-coat-of-arms"
-        src="leinster-coat-of-arms.svg"
-        text="coat of arms of leinster"
-      />
-      <Target
-        label="leinster-coat-of-arms"
-        src="leinster-coat-of-arms.svg"
-        text="coat of arms of leinster"
-      />
+      <Target label="xbox" src="xbox.svg" text="xbox logo" />
+      <Target label="two-cars" src="two-cars.webp" text="two cars" />
       <button type="submit">submit</button>
     </form>
   );
